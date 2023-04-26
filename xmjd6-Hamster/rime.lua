e60e7c0bccfcbc889c85ca783c53bc2bfafefa53
fusheng <200679732@outlook.com>
2023-04-27 07:08:00 +0800

    firstcommit

---
+++ xmjd6-Hamster/rime.lua
@@ -0,0 +1,20 @@
+-- 星猫键道6
+-- xmjd6_filter: 单字模式 & 630 即 ss 词组提示
+--- 修改自 @懒散 TsFreddie https://github.com/TsFreddie/jdc_lambda/blob/master/rime/lua/xkjdc_sbb_hint.lua
+-- 可由 schema 的 danzi_mode 与 wxw_hint 开关控制
+-- 详见 `lua/xmjd6_filter.lua`
+xmjd6_filter = require("xmjd6_filter")
+-- 顶功处理器
+xmjdtopup_processor = require("xmjdfor_topup")
+-- 声笔笔简码提示 | 顶功提示 | 补全处理
+xmjdhint_filter = require("xmjdfor_hint")
+-- number_translator: 将 `=` + 阿拉伯数字 翻译为大小写汉字
+-- 详见 `lua/number.lua`
+xmjdnumber_translator = require("xmjdxnumber")
+-- 用 ' 作为次选键
+xmjdsmart_2 = require("xmjdsmart_2")
+xmjdshuzi = require("xmjdshuzi")
+xmjdjisuanqi = require("xmjdjisuanqi")
+xmjdshijian = require("xmjdshijian")
+xmjdxuanchongjsq = require("xmjdxuanchongjsq")
+
