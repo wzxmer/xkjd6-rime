51be7f0443a6058f3d9a8ef48e47519c47c9e0a0
fusheng <200679732@outlook.com>
2023-10-07 11:45:38 +0800

    更新若干+修复

--- xmjd6/rime.lua
+++ xmjd6/rime.lua
@@ -11,3 +11,5 @@ xmjdsmart_2 = require("xmjdsmart_2")
 xmjdshuzi = require("xmjdshuzi")
 xmjdjisuanqi = require("xmjdjisuanqi")
 xmjdshijian = require("xmjdshijian")
+--内嵌脚本
+embeded_cands = require("embeded_cands")
